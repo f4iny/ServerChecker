3da01ef8b02d39e1dcfce0451f44b56b4395b436
fainy <fainy@email.org>
2026-04-22 15:08:30 +0700

    добавлена ошибка если не нашелся текст сред. значения пинга

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from subprocess import run as subprocess_run
+from re import search as re_search
 
 
 print("IP-адрес вида x.x.x.x")
-# server_ip = input("Введите ip адрес сервера: ")
-server_ip = "144.31.134.15"
+server_ip = input("Введите ip адрес сервера: ")
 default_server_ip = "144.31.134.15"
 
 
@@ -11,8 +11,13 @@ def ping(server_ip=default_server_ip):
     text = subprocess_run(
         ["ping", server_ip], capture_output=True, text=True, encoding="cp866"
     ).stdout
-    return text[text.find("Среднее = ") + 10 :]
+    finded_text = re_search(r"Среднее = \d+ мсек", text)
+    if finded_text is None:
+        raise TypeError("Ошибка в поиске среднего значения")
+    else:
+        avg = finded_text.group()
+        return avg[avg.find("=") + 2 :]
 
 
-print(ping(server_ip))
+print(ping())  # пока без параметра сервера, т.к. лень вводить его каждый раз.
 # Пинг запихнуть в отдельную функцию которая выводит среднее ms значение, функцию можно касмотризировать.
